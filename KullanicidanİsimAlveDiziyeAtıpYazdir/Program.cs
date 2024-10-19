9afdbe48ebe7f22f1e78921a657cad6185b0e2e6
evinyanitoz <eyanitoz@gmail.com>
2024-10-19 19:03:32 +0300

    Add project files.

---
+++ KullanicidanİsimAlveDiziyeAtıpYazdir/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace KullanicidanİsimAlveDiziyeAtıpYazdir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac = 0;
+            string isim;
+            List<string> isimler = new List<string>();
+            do
+            {
+                sayac++;
+                Console.WriteLine($"{sayac}.İsimi Giriniz :");
+
+                isim = Console.ReadLine();
+                isimler.Add(isim);
+
+            }
+            while (isim != "");
+
+            Console.WriteLine("OLUŞTURDUĞUN DİZİ ;");
+
+            foreach (var array in isimler)
+            {
+
+                Console.WriteLine($"{array}");
+
+            }
+        }
+    }
+}
